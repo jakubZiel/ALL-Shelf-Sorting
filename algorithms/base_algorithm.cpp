c8a580e6122173258ced4309f3cd14a1a6b5983f
Adam <narozniak.adam4@gmail.com>
2021-06-13 00:28:57 +0200

    Base, naive and four first algorithm added. Step by step not fully working

---
+++ algorithms/base_algorithm.cpp
@@ -0,0 +1,257 @@
+#include <iostream>
+#include <algorithm>
+#include "base_algorithm.h"
+
+/**
+ * Implement counter and basic functions needed for algorithms.
+ */
+
+Base::Base(std::vector<int> &shelf){
+    _shelf = shelf;
+    _shelf_size = shelf.size();
+    _shelf_tab = new int[_shelf_size];
+    _shelf_copy = new int[_shelf_size];
+    _count_colors();
+    _first_unsorted_index = 0;
+}
+
+Base::~Base(){
+    delete[] _shelf_tab;
+}
+void Base::_count_colors() {
+    for (int & i : _counter) {
+        i = 0;
+    }
+    int j = 0;
+    for (auto i : _shelf) {
+        ++_counter[i];
+        _shelf_tab[j] = i;
+        _shelf_copy[j++] = i;
+    }
+}
+int Base::_find_color_pos(int color) {
+    int i = _first_unsorted_index;
+    while (i < _shelf_size && _shelf_tab[i] != color)
+        ++i;
+    return i;
+}
+void Base::_move_to_beg(int color_first_occurrence_index) {
+    if (color_first_occurrence_index == _first_unsorted_index)
+        return;
+
+    int num = (_shelf_size - 1) - _first_unsorted_index;
+
+    if (num > 6) {
+        if ((color_first_occurrence_index - _first_unsorted_index) % 4 == 0) {
+            for (int i = 0; i < (color_first_occurrence_index - _first_unsorted_index) / 4; i++) {
+                _move(_first_unsorted_index);
+            }
+        } else {
+            if (num % 4 == 0) {
+                if (color_first_occurrence_index + 4 >= _shelf_size) {
+                    _move(_first_unsorted_index);
+                    color_first_occurrence_index -= 4;
+                }
+
+                if (color_first_occurrence_index - 3 < _first_unsorted_index) {
+                    _move(color_first_occurrence_index);
+                    color_first_occurrence_index = _shelf_size - 4;
+                }
+
+                _move(color_first_occurrence_index - 3);
+                for (int i = 0; i < num / 4; i++) {
+                    _move(_first_unsorted_index);
+                }
+            } else if (num % 4 == 1) {
+                if (color_first_occurrence_index + 4 >= _shelf_size) {
+                    _move(_first_unsorted_index);
+                    color_first_occurrence_index -= 4;
+                }
+
+                if (color_first_occurrence_index - 2 < _first_unsorted_index) {
+                    _move(color_first_occurrence_index);
+                    color_first_occurrence_index = _shelf_size - 4;
+                }
+
+                _move(color_first_occurrence_index - 2);
+                for (int i = 0; i < num / 4; i++) {
+                    _move(_first_unsorted_index);
+                }
+            } else if (num % 4 == 2) {
+                if (color_first_occurrence_index + 4 >= _shelf_size) {
+                    _move(_first_unsorted_index);
+                    color_first_occurrence_index -= 4;
+                }
+
+                if (color_first_occurrence_index - 1 < _first_unsorted_index) {
+                    _move(color_first_occurrence_index);
+                    color_first_occurrence_index = _shelf_size - 4;
+                }
+
+                _move(color_first_occurrence_index - 1);
+                for (int i = 0; i < num / 4; i++) {
+                    _move(_first_unsorted_index);
+                }
+            } else {
+                if (color_first_occurrence_index + 4 >= _shelf_size) {
+                    _move(_first_unsorted_index);
+                    color_first_occurrence_index -= 4;
+                }
+
+                _move(color_first_occurrence_index);
+                for (int i = 0; i < num / 4; i++) {
+                    _move(_first_unsorted_index);
+                }
+            }
+        }
+    } else if (num == 6) {
+        switch (color_first_occurrence_index - _first_unsorted_index) {
+            case 1:
+                _move(_first_unsorted_index);
+                _move(_first_unsorted_index);
+
+                break;
+
+            case 2:
+                _move(_first_unsorted_index);
+                _move(_first_unsorted_index + 1);
+                _move(_first_unsorted_index);
+                _move(_first_unsorted_index);
+
+                break;
+
+            case 3:
+                _move(_first_unsorted_index + 2);
+                _move(_first_unsorted_index);
+
+                break;
+
+            case 4:
+                _move(_first_unsorted_index);
+
+                break;
+
+            case 5:
+                _move(_first_unsorted_index + 1);
+                _move(_first_unsorted_index);
+                _move(_first_unsorted_index);
+
+                break;
+
+            case 6:
+                _move(_first_unsorted_index);
+                _move(_first_unsorted_index);
+                _move(_first_unsorted_index + 1);
+                _move(_first_unsorted_index);
+                _move(_first_unsorted_index);
+
+                break;
+            default:
+                break;
+        }
+    } else if (num == 5) {
+        switch (color_first_occurrence_index - _first_unsorted_index) {
+            case 1:
+                _move(_first_unsorted_index);
+                _move(_first_unsorted_index + 1);
+                _move(_first_unsorted_index);
+
+                break;
+
+            case 2:
+                _move(_first_unsorted_index);
+                _move(_first_unsorted_index);
+
+                break;
+
+            case 3:
+                _move(_first_unsorted_index + 1);
+                _move(_first_unsorted_index);
+
+                break;
+
+            case 4:
+                _move(_first_unsorted_index);
+
+                break;
+
+            case 5:
+                _move(_first_unsorted_index);
+                _move(_first_unsorted_index);
+                _move(_first_unsorted_index + 1);
+                _move(_first_unsorted_index);
+
+                break;
+            default:
+                break;
+        }
+    }
+}
+void Base::_move(int color_first_occurrence_index) {
+    _moves_history.push_back(color_first_occurrence_index);
+    int A, B, C, D;
+    A = _shelf_tab[color_first_occurrence_index];
+    B = _shelf_tab[color_first_occurrence_index + 1];
+    C = _shelf_tab[color_first_occurrence_index + 2];
+    D = _shelf_tab[color_first_occurrence_index + 3];
+
+    for (int i = color_first_occurrence_index + 4; i < _shelf_size; i++) {
+        _shelf_tab[i - 4] = _shelf_tab[i];
+    }
+
+    _shelf_tab[_shelf_size - 4] = A;
+    _shelf_tab[_shelf_size - 3] = B;
+    _shelf_tab[_shelf_size - 2] = C;
+    _shelf_tab[_shelf_size - 1] = D;
+}
+void Base::_move_copy(int color_first_occurrence_index){
+    int A, B, C, D;
+    A = _shelf_copy[color_first_occurrence_index];
+    B = _shelf_copy[color_first_occurrence_index + 1];
+    C = _shelf_copy[color_first_occurrence_index + 2];
+    D = _shelf_copy[color_first_occurrence_index + 3];
+
+    for (int i = color_first_occurrence_index + 4; i < _shelf_size; i++) {
+        _shelf_copy[i - 4] = _shelf_copy[i];
+    }
+
+    _shelf_copy[_shelf_size - 4] = A;
+    _shelf_copy[_shelf_size - 3] = B;
+    _shelf_copy[_shelf_size - 2] = C;
+    _shelf_copy[_shelf_size - 1] = D;
+};
+void Base::_show_single_step() {
+    std::string ret = "";
+    for (int i = 0; i < _shelf_size; i++) {
+        if (_shelf_tab[i] == 0)
+            ret += "C";
+        else if (_shelf_tab[i] == 1)
+            ret += "M";
+        else if (_shelf_tab[i] == 2) {
+            ret += "Y";
+        } else if (_shelf_tab[i] == 3) {
+            ret += "K";
+        }
+    }
+    std::cout << ret << std::endl;
+}
+void Base::show_step_by_step() {
+    int i = 0;
+    for (auto pos : _moves_history) {
+        _move_copy(pos);
+        std::cout << i++ << ") ";
+        _show_single_step();
+        _print_indicator(pos);
+    }
+}
+
+void Base::_print_indicator(int current_index){
+    std::cout<<"   ";
+    for (int i = 0; i < _shelf_size; i++){
+        if (i == current_index)
+            std::cout << "^";
+        else
+            std::cout << " ";
+    }
+    std::cout << std::endl;
+}
--- main.cpp
+++ main.cpp
@@ -1,36 +1,46 @@
 #include <iostream>
-#include "algorithms/BruteForce.h"
+//#include "algorithms/BruteForce.h"
 #include "input_generator/input_generator.h"
-void test_brute_force(){
-    std::vector<int> test = {3,0,0,0,3};
-
-    std::vector<int> shelf = {2,2,3,3,3,3,3,3,3,2,0,3,2,2,2};
-
-    std::vector<int> shelf1 = {1,1,1,1,1,1,3,3,3,3,3,3,3,3,3,3,0,0,0,0,0};
-
-    std::vector<int> shelf2 = {1,3,3,1,1,1,1,1,0,0,3,1,1,1,1,1,1,1,3,2,2,1,3,3,3};
-
-    std::vector<int> shelf3 = {0,3,3,0,1,1,2,3,3,3,3,3,1,1,3,3,2,0,0,2};
-
-    std::vector<int> shelf4 = {1,3,3,1,0,3,2,3,2,3};
-
-    BruteForce brute(shelf);
-
-    auto result = brute.sort();
-
-    brute.showStepByStep(result);
-}
+#include "algorithms/four_first_algorithm.h"
+#include "algorithms/naive.h"
+//void test_brute_force(){
+//    std::vector<int> test = {3,0,0,0,3};
+//
+//    std::vector<int> shelf = {2,2,3,3,3,3,3,3,3,2,0,3,2,2,2};
+//
+//    std::vector<int> shelf1 = {1,1,1,1,1,1,3,3,3,3,3,3,3,3,3,3,0,0,0,0,0};
+//
+//    std::vector<int> shelf2 = {1,3,3,1,1,1,1,1,0,0,3,1,1,1,1,1,1,1,3,2,2,1,3,3,3};
+//
+//    std::vector<int> shelf3 = {0,3,3,0,1,1,2,3,3,3,3,3,1,1,3,3,2,0,0,2};
+//
+//    std::vector<int> shelf4 = {1,3,3,1,0,3,2,3,2,3};
+//
+//    BruteForce brute(shelf);
+//
+//    auto result = brute.sort();
+//
+//    brute.showStepByStep(result);
+//}
 void test_input_generator(){
     InputGenerator inputGenerator(15);
     inputGenerator.gen_same_next(0.8);
     auto color_vector = inputGenerator.get_color_vector();
-    for (int i = 0; i< 15; i++){
-        std::cout<<color_vector[i]<<std::endl;
-    }
+//    for (int i = 0; i< 15; i++){
+//        std::cout<<color_vector[i]<<std::endl;
+//    }
 }
 
 int main() {
     test_input_generator();
+    InputGenerator inputGenerator(20);
+    inputGenerator.gen_same_next(0.8);
+    auto color_vector = inputGenerator.get_color_vector();
+//    Naive naive(color_vector);
+//    naive.sort();
+    FourFirst fourFirst(color_vector);
+    fourFirst.sort();
+    fourFirst.show_step_by_step();
     return 0;
 }
 
